2c3f357d8db76b4a8dd2e8b7a03d44c5896c3ab2
Izorkin <Izorkin@gmail.com>
2018-08-03 23:47:14 +0300

    mariadb.galera: init plugin galera 25.3.23

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ rec {
 
   multichain       = pkgs.callPackage ./pkgs/apps/altcoins/multichain.nix { };
   omnicore         = pkgs.callPackage ./pkgs/apps/altcoins/omnicore.nix { };
-  mariadb          = pkgs.callPackage ./pkgs/servers/mariadb { openssl = pkgs.libressl; inherit (pkgs.darwin) cctools; inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices; };
+  mariadb          = pkgs.callPackage ./pkgs/servers/mariadb { openssl = pkgs.libressl; asio = pkgs.asio_1_10; inherit (pkgs.darwin) cctools; inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices; };
   oh-my-zsh-custom = pkgs.callPackage ./pkgs/shells/oh-my-zsh-custom { };
   fail2ban         = pkgs.callPackage ./pkgs/tools/fail2ban { };
 }
