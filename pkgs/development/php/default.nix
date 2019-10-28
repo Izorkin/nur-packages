24808d255a20c1cb315cef5a75d49b63185d4328
Izorkin <izorkin@elven.pw>
2019-10-28 12:36:59 +0300

    php72: 7.2.23 -> 7.2.24

--- pkgs/development/php/default.nix
+++ pkgs/development/php/default.nix
@@ -275,8 +275,8 @@ in {
   };
 
   php72 = generic {
-    version = "7.2.23";
-    sha256 = "03a3snx8wdn2pwfy8qdk035da9g3qdnpgqvpz4qfgmr97mjg6ym1";
+    version = "7.2.24";
+    sha256 = "00znhjcn6k4mbxz6jqlqf6bzr4cqdf8pnbmxkg6bns1hnr6r6yd0";
 
     # https://bugs.php.net/bug.php?id=76826
     extraPatches = optional stdenv.isDarwin ./php72-darwin-isfinite.patch;
