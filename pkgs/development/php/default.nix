55bb2a5e97b7869e2519d5b57b707d6be446937c
Izorkin <izorkin@elven.pw>
2020-05-15 12:59:51 +0300

    php73: 7.3.17 -> 7.3.18

--- pkgs/development/php/default.nix
+++ pkgs/development/php/default.nix
@@ -352,8 +352,8 @@ in {
   };
 
   php73 = generic {
-    version = "7.3.17";
-    sha256 = "02cplaja13l9j2abfsn6cxd2kryhw7a2wwrkk4h9z6ac0bcr0gnq";
+    version = "7.3.18";
+    sha256 = "19787fjz3p195dhq0bk7dcn76m6vyl2ng12zawxiamzbbpv237bl";
 
     # https://bugs.php.net/bug.php?id=76826
     extraPatches = optional stdenv.isDarwin ./patch/php73-darwin-isfinite.patch;
