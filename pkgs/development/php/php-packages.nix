aa50423389960b80e220bfb75542d92979c4e509
Izorkin <izorkin@elven.pw>
2020-05-21 18:30:47 +0300

    phpPackages.event: 2.5.5 -> 2.5.6

--- pkgs/development/php/php-packages.nix
+++ pkgs/development/php/php-packages.nix
@@ -186,10 +186,10 @@ let
   };
 
   event = buildPecl {
-    version = "2.5.5";
+    version = "2.5.6";
     pname = "event";
 
-    sha256 = "0cwx2d0kq7cnl2lh3jfz0jw4024szqb4vm0v8b0r35gz571j7dsr";
+    sha256 = "16qxdpm55birp7vpwjcbx35a5c1rfz1bcvqg4jm2k5y85vdjnwfz";
 
     configureFlags = with pkgs; [
       "--with-event-libevent-dir=${libevent.dev}"
