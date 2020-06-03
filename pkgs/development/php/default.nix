5b26dac0fc639f9684666998049e8f0031433bb7
Izorkin <izorkin@elven.pw>
2020-06-03 16:30:33 +0300

    php: sort configuration flags and options

--- pkgs/development/php/default.nix
+++ pkgs/development/php/default.nix
@@ -19,56 +19,66 @@ let
   { version
   , sha256
   , extraPatches ? []
-  , withSystemd ? config.php.systemd or stdenv.isLinux
-  , imapSupport ? config.php.imap or (!stdenv.isDarwin)
-  , ldapSupport ? config.php.ldap or true
-  , mhashSupport ? (config.php.mhash or true) && (versionOlder version "7.0")
-  , mysqlndSupport ? config.php.mysqlnd or true
-  , mysqliSupport ? (config.php.mysqli or true) && (mysqlndSupport)
-  , pdo_mysqlSupport ? (config.php.pdo_mysql or true) && (mysqlndSupport)
-  , libxml2Support ? config.php.libxml2 or true
+
+  # Sapi flags
   , apxs2Support ? config.php.apxs2 or (!stdenv.isDarwin)
-  , embedSupport ? config.php.embed or false
-  , bcmathSupport ? config.php.bcmath or true
-  , socketsSupport ? config.php.sockets or true
-  , curlSupport ? config.php.curl or true
-  , gettextSupport ? config.php.gettext or true
-  , pcntlSupport ? config.php.pcntl or true
-  , pdo_odbcSupport ? config.php.pdo_odbc or true
-  , postgresqlSupport ? config.php.postgresql or true
-  , pdo_pgsqlSupport ? config.php.pdo_pgsql or true
-  , readlineSupport ? config.php.readline or true
-  , sqliteSupport ? config.php.sqlite or true
-  , soapSupport ? (config.php.soap or true) && (libxml2Support)
-  , zlibSupport ? config.php.zlib or true
-  , opensslSupport ? config.php.openssl or true
-  , mbstringSupport ? config.php.mbstring or true
-  , gdSupport ? config.php.gd or true
-  , intlSupport ? config.php.intl or true
-  , exifSupport ? config.php.exif or true
-  , xslSupport ? config.php.xsl or false
-  , mcryptSupport ? (config.php.mcrypt or true) && (versionOlder version "7.2")
-  , bz2Support ? config.php.bz2 or false
-  , zipSupport ? config.php.zip or true
-  , ftpSupport ? config.php.ftp or true
-  , fpmSupport ? config.php.fpm or true
-  , gmpSupport ? config.php.gmp or true
-  , mssqlSupport ? (config.php.mssql or (!stdenv.isDarwin)) && (!php7)
-  , ztsSupport ? (config.php.zts or false) || (apxs2Support)
-  , calendarSupport ? config.php.calendar or true
-  , sodiumSupport ? (config.php.sodium or true) && (versionAtLeast version "7.2")
-  , tidySupport ? (config.php.tidy or false)
-  , argon2Support ? (config.php.argon2 or true) && (versionAtLeast version "7.2")
-  , libzipSupport ? (config.php.libzip or true) && (versionAtLeast version "7.2")
-  , phpdbgSupport ? config.php.phpdbg or true
   , cgiSupport ? config.php.cgi or true
   , cliSupport ? config.php.cli or true
-  , pharSupport ? config.php.phar or true
-  , xmlrpcSupport ? (config.php.xmlrpc or false) && (libxml2Support)
+  , embedSupport ? config.php.embed or false
+  , fpmSupport ? config.php.fpm or true
+  , phpdbgSupport ? config.php.phpdbg or true
+
+  # Misc flags
+  , argon2Support ? (config.php.argon2 or true) && (versionAtLeast version "7.2")
   , cgotoSupport ? config.php.cgoto or false
-  , valgrindSupport ? (config.php.valgrind or true) && (versionAtLeast version "7.2")
   , ipv6Support ? config.php.ipv6 or true
   , pearSupport ? (config.php.pear or true) && (libxml2Support)
+  , valgrindSupport ? (config.php.valgrind or true) && (versionAtLeast version "7.2")
+  , withSystemd ? config.php.systemd or stdenv.isLinux
+  , ztsSupport ? (config.php.zts or false) || (apxs2Support)
+
+  # Extension flags only in 5.6
+  , mssqlSupport ? (config.php.mssql or (!stdenv.isDarwin)) && (!php7)
+
+  # Extension flags in 5.6 and higher
+  , bcmathSupport ? config.php.bcmath or true
+  , bz2Support ? config.php.bz2 or false
+  , calendarSupport ? config.php.calendar or true
+  , curlSupport ? config.php.curl or true
+  , exifSupport ? config.php.exif or true
+  , ftpSupport ? config.php.ftp or true
+  , gdSupport ? config.php.gd or true
+  , gettextSupport ? config.php.gettext or true
+  , gmpSupport ? config.php.gmp or true
+  , mhashSupport ? (config.php.mhash or true) && (versionOlder version "7.0")
+  , imapSupport ? config.php.imap or (!stdenv.isDarwin)
+  , intlSupport ? config.php.intl or true
+  , ldapSupport ? config.php.ldap or true
+  , libxml2Support ? config.php.libxml2 or true
+  , mbstringSupport ? config.php.mbstring or true
+  , mcryptSupport ? (config.php.mcrypt or true) && (versionOlder version "7.2")
+  , mysqliSupport ? (config.php.mysqli or true) && (mysqlndSupport)
+  , mysqlndSupport ? config.php.mysqlnd or true
+  , opensslSupport ? config.php.openssl or true
+  , pcntlSupport ? config.php.pcntl or true
+  , pdo_mysqlSupport ? (config.php.pdo_mysql or true) && (mysqlndSupport)
+  , pdo_odbcSupport ? config.php.pdo_odbc or true
+  , pdo_pgsqlSupport ? config.php.pdo_pgsql or true
+  , sqliteSupport ? config.php.sqlite or true
+  , postgresqlSupport ? config.php.postgresql or true
+  , pharSupport ? config.php.phar or true
+  , readlineSupport ? config.php.readline or true
+  , soapSupport ? (config.php.soap or true) && (libxml2Support)
+  , socketsSupport ? config.php.sockets or true
+  , tidySupport ? (config.php.tidy or false)
+  , xmlrpcSupport ? (config.php.xmlrpc or false) && (libxml2Support)
+  , xslSupport ? config.php.xsl or false
+  , zipSupport ? config.php.zip or true
+  , libzipSupport ? (config.php.libzip or true) && (versionAtLeast version "7.2")
+  , zlibSupport ? config.php.zlib or true
+
+  # Extension flags in 7.2 and higher
+  , sodiumSupport ? (config.php.sodium or true) && (versionAtLeast version "7.2")
   }:
 
     let
@@ -87,53 +97,116 @@ let
         ++ optional (versionAtLeast version "7.0") bison;
 
       buildInputs = [ ]
-        ++ optional (versionOlder version "7.3") pcre
-        ++ optional (versionAtLeast version "7.3") pcre2
-        ++ optional (versionAtLeast version "7.4") oniguruma
-        ++ optional withSystemd systemd
-        ++ optionals imapSupport [ uwimap openssl pam ]
-        ++ optionals curlSupport [ curl openssl ]
-        ++ optionals ldapSupport [ openldap openssl ]
-        ++ optionals gdSupport [ gd freetype libXpm libjpeg libpng libwebp ]
-        ++ optionals opensslSupport [ openssl openssl.dev ]
+        # Sapi flags
         ++ optional apxs2Support apacheHttpd
-        ++ optional (ldapSupport && stdenv.isLinux) cyrus_sasl
-        ++ optional mhashSupport libmhash
-        ++ optional zlibSupport zlib
-        ++ optional libxml2Support libxml2
-        ++ optional readlineSupport readline
-        ++ optional sqliteSupport sqlite
-        ++ optional postgresqlSupport postgresql
-        ++ optional pdo_odbcSupport unixODBC
-        ++ optional pdo_pgsqlSupport postgresql
-        ++ optional gmpSupport gmp
+
+        # Misc flags
+        ++ optional argon2Support libargon2
+        ++ optional valgrindSupport valgrind
+        ++ optional withSystemd systemd
+
+        # Extension flags only in 5.6
+        ++ optional (mssqlSupport && !stdenv.isDarwin) freetds
+
+        # Extension flags in 5.6 and higher
+        ++ optional bz2Support bzip2
+        ++ optionals curlSupport [ curl openssl ]
+        ++ optionals gdSupport [ gd freetype libXpm libjpeg libpng libwebp ]
         ++ optional gettextSupport gettext
+        ++ optional gmpSupport gmp
+        ++ optional mhashSupport libmhash
+        ++ optionals imapSupport [ uwimap openssl pam ]
         ++ optional (intlSupport && (versionOlder version "7.0")) icu60
         ++ optional (intlSupport && (versionAtLeast version "7.0")) icu
-        ++ optional xslSupport libxslt
+        ++ optionals ldapSupport [ openldap openssl ]
+        ++ optional (ldapSupport && stdenv.isLinux) cyrus_sasl
+        ++ optional libxml2Support libxml2
         ++ optional mcryptSupport libmcrypt'
-        ++ optional bz2Support bzip2
-        ++ optional (mssqlSupport && !stdenv.isDarwin) freetds
-        ++ optional sodiumSupport libsodium
+        ++ optionals opensslSupport [ openssl openssl.dev ]
+        ++ optional (versionOlder version "7.3") pcre
+        ++ optional (versionAtLeast version "7.3") pcre2
+        ++ optional pdo_odbcSupport unixODBC
+        ++ optional pdo_pgsqlSupport postgresql
+        ++ optional sqliteSupport sqlite
+        ++ optional postgresqlSupport postgresql
+        ++ optional readlineSupport readline
         ++ optional tidySupport html-tidy
-        ++ optional argon2Support libargon2
+        ++ optional xslSupport libxslt
+        ++ optional zlibSupport zlib
         ++ optional libzipSupport libzip
-        ++ optional valgrindSupport valgrind;
+
+        # Extension flags in 7.2 and higher
+        ++ optional sodiumSupport libsodium
+
+        # Extension flags in 7.4 and higher
+        ++ optional (versionAtLeast version "7.4") oniguruma;
 
       CXXFLAGS = optionalString stdenv.cc.isClang "-std=c++11";
 
       configureFlags = [
         "--with-config-file-scan-dir=/etc/php.d"
       ]
+
+      # PCRE
       ++ optionals (versionOlder version "7.3") [ "--with-pcre-regex=${pcre.dev}" "PCRE_LIBDIR=${pcre}" ]
       ++ optionals (versions.majorMinor version == "7.3") [ "--with-pcre-regex=${pcre2.dev}" "PCRE_LIBDIR=${pcre2}" ]
       ++ optionals (versionAtLeast version "7.4") [ "--with-external-pcre=${pcre2.dev}" "PCRE_LIBDIR=${pcre2}" ]
-      ++ optional stdenv.isDarwin "--with-iconv=${libiconv}"
+
+      # Enable sapis
+      ++ optional apxs2Support "--with-apxs2=${apacheHttpd.dev}/bin/apxs"
+      ++ optional (!cgiSupport) "--disable-cgi"
+      ++ optional (!cliSupport) "--disable-cli"
+      ++ optional embedSupport "--enable-embed"
+      ++ optional fpmSupport "--enable-fpm"
+      ++ optional phpdbgSupport "--enable-phpdbg"
+      ++ optional (!phpdbgSupport) "--disable-phpdbg"
+
+      # Misc flags
+      ++ optional argon2Support "--with-password-argon2=${libargon2}"
+      ++ optional cgotoSupport "--enable-re2c-cgoto"
+      ++ optional (!ipv6Support) "--disable-ipv6"
+      ++ optional (pearSupport && libxml2Support) "--with-pear=$(out)/lib/php/pear"
+      ++ optional valgrindSupport "--with-valgrind=${valgrind.dev}"
       ++ optional withSystemd "--with-fpm-systemd"
+      ++ optional ztsSupport "--enable-maintainer-zts"
+      ++ optional stdenv.isDarwin "--with-iconv=${libiconv}"
+
+      # Enable extensions only in 5.6
+      ++ optional (mssqlSupport && !stdenv.isDarwin) "--with-mssql=${freetds}"
+
+      # Enable Extensions in 5.6 and higher
+      ++ optional bcmathSupport "--enable-bcmath"
+      ++ optional bz2Support "--with-bz2=${bzip2.dev}"
+      ++ optional calendarSupport "--enable-calendar"
+      ++ optional curlSupport "--with-curl=${curl.dev}"
+      ++ optional exifSupport "--enable-exif"
+      ++ optional ftpSupport "--enable-ftp"
+      ++ optionals (gdSupport && versionOlder version "7.4") [
+        "--with-gd=${gd.dev}"
+        (if (versionAtLeast version "7.0") then "--with-webp-dir=${libwebp}" else null)
+        "--with-jpeg-dir=${libjpeg.dev}"
+        "--with-png-dir=${libpng.dev}"
+        "--with-freetype-dir=${freetype.dev}"
+        "--with-xpm-dir=${libXpm.dev}"
+        "--enable-gd-jis-conv"
+      ]
+      ++ optionals (gdSupport && versionAtLeast version "7.4") [
+        "--enable-gd"
+        "--with-external-gd=${gd.dev}"
+        "--with-webp=${libwebp}"
+        "--with-jpeg=${libjpeg.dev}"
+        "--with-xpm=${libXpm.dev}"
+        "--with-freetype=${freetype.dev}"
+        "--enable-gd-jis-conv"
+      ]
+      ++ optional gettextSupport "--with-gettext=${gettext}"
+      ++ optional gmpSupport "--with-gmp=${gmp.dev}"
+      ++ optional mhashSupport "--with-mhash"
       ++ optionals imapSupport [
         "--with-imap=${uwimap}"
         "--with-imap-ssl"
       ]
+      ++ optional intlSupport "--enable-intl"
       ++ optionals ldapSupport [
         "--with-ldap=/invalid/path"
         "LDAP_DIR=${openldap.dev}"
@@ -141,11 +214,6 @@ let
         "LDAP_LIBDIR=${openldap.out}/lib"
       ]
       ++ optional (ldapSupport && stdenv.isLinux) "--with-ldap-sasl=${cyrus_sasl.dev}"
-      ++ optional apxs2Support "--with-apxs2=${apacheHttpd.dev}/bin/apxs"
-      ++ optional embedSupport "--enable-embed"
-      ++ optional mhashSupport "--with-mhash"
-      ++ optional curlSupport "--with-curl=${curl.dev}"
-      ++ optional zlibSupport "--with-zlib=${zlib.dev}"
       ++ optional (libxml2Support && (versionOlder version "7.4")) "--with-libxml-dir=${libxml2.dev}"
       ++ optional (!libxml2Support) [
         "--disable-dom"
@@ -156,65 +224,31 @@ let
         "--disable-xmlwriter"
         "--without-pear"
       ]
+      ++ optional mbstringSupport "--enable-mbstring"
+      ++ optional mcryptSupport "--with-mcrypt=${libmcrypt'}"
+      ++ optional (mysqliSupport && mysqlndSupport) "--with-mysqli=mysqlnd"
+      ++ optional opensslSupport "--with-openssl"
       ++ optional pcntlSupport "--enable-pcntl"
-      ++ optional readlineSupport "--with-readline=${readline.dev}"
-      ++ optional sqliteSupport "--with-pdo-sqlite=${sqlite.dev}"
-      ++ optional postgresqlSupport "--with-pgsql=${postgresql}"
+      ++ optional (pdo_mysqlSupport && mysqlndSupport) "--with-pdo-mysql=mysqlnd"
+      ++ optional (pdo_mysqlSupport || mysqliSupport) "--with-mysql-sock=/run/mysqld/mysqld.sock"
       ++ optional pdo_odbcSupport "--with-pdo-odbc=unixODBC,${unixODBC}"
       ++ optional pdo_pgsqlSupport "--with-pdo-pgsql=${postgresql}"
-      ++ optional (pdo_mysqlSupport && mysqlndSupport) "--with-pdo-mysql=mysqlnd"
-      ++ optional (mysqliSupport && mysqlndSupport) "--with-mysqli=mysqlnd"
-      ++ optional (pdo_mysqlSupport || mysqliSupport) "--with-mysql-sock=/run/mysqld/mysqld.sock"
-      ++ optional bcmathSupport "--enable-bcmath"
-      ++ optionals (gdSupport && versionAtLeast version "7.4") [
-        "--enable-gd"
-        "--with-external-gd=${gd.dev}"
-        "--with-webp=${libwebp}"
-        "--with-jpeg=${libjpeg.dev}"
-        "--with-xpm=${libXpm.dev}"
-        "--with-freetype=${freetype.dev}"
-        "--enable-gd-jis-conv"
-      ] ++ optionals (gdSupport && versionOlder version "7.4") [
-        "--with-gd=${gd.dev}"
-        (if (versionAtLeast version "7.0") then "--with-webp-dir=${libwebp}" else null)
-        "--with-jpeg-dir=${libjpeg.dev}"
-        "--with-png-dir=${libpng.dev}"
-        "--with-freetype-dir=${freetype.dev}"
-        "--with-xpm-dir=${libXpm.dev}"
-        "--enable-gd-jis-conv"
-      ]
-      ++ optional gmpSupport "--with-gmp=${gmp.dev}"
+      ++ optional sqliteSupport "--with-pdo-sqlite=${sqlite.dev}"
+      ++ optional postgresqlSupport "--with-pgsql=${postgresql}"
+      ++ optional (!pharSupport) "--disable-phar"
+      ++ optional readlineSupport "--with-readline=${readline.dev}"
       ++ optional soapSupport "--enable-soap"
       ++ optional socketsSupport "--enable-sockets"
-      ++ optional opensslSupport "--with-openssl"
-      ++ optional mbstringSupport "--enable-mbstring"
-      ++ optional gettextSupport "--with-gettext=${gettext}"
-      ++ optional intlSupport "--enable-intl"
-      ++ optional exifSupport "--enable-exif"
+      ++ optional tidySupport "--with-tidy=${html-tidy}"
+      ++ optional xmlrpcSupport "--with-xmlrpc"
       ++ optional xslSupport "--with-xsl=${libxslt.dev}"
-      ++ optional mcryptSupport "--with-mcrypt=${libmcrypt'}"
-      ++ optional bz2Support "--with-bz2=${bzip2.dev}"
       ++ optional (zipSupport && (versionOlder version "7.4")) "--enable-zip"
       ++ optional (zipSupport && (versionAtLeast version "7.4")) "--with-zip"
-      ++ optional ftpSupport "--enable-ftp"
-      ++ optional fpmSupport "--enable-fpm"
-      ++ optional (mssqlSupport && !stdenv.isDarwin) "--with-mssql=${freetds}"
-      ++ optional ztsSupport "--enable-maintainer-zts"
-      ++ optional calendarSupport "--enable-calendar"
-      ++ optional sodiumSupport "--with-sodium=${libsodium.dev}"
-      ++ optional tidySupport "--with-tidy=${html-tidy}"
-      ++ optional argon2Support "--with-password-argon2=${libargon2}"
       ++ optional (libzipSupport && (versionOlder version "7.4")) "--with-libzip=${libzip.dev}"
-      ++ optional phpdbgSupport "--enable-phpdbg"
-      ++ optional (!phpdbgSupport) "--disable-phpdbg"
-      ++ optional (!cgiSupport) "--disable-cgi"
-      ++ optional (!cliSupport) "--disable-cli"
-      ++ optional (!pharSupport) "--disable-phar"
-      ++ optional xmlrpcSupport "--with-xmlrpc"
-      ++ optional cgotoSupport "--enable-re2c-cgoto"
-      ++ optional valgrindSupport "--with-valgrind=${valgrind.dev}"
-      ++ optional (!ipv6Support) "--disable-ipv6"
-      ++ optional (pearSupport && libxml2Support) "--with-pear=$(out)/lib/php/pear";
+      ++ optional zlibSupport "--with-zlib=${zlib.dev}"
+
+      # Enable extensions in 7.2 and higher
+      ++ optional sodiumSupport "--with-sodium=${libsodium.dev}";
 
       hardeningDisable = [ "bindnow" ];
 
