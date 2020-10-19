72aa3df22affb4600cca6c868c06aaaeaaa95fb9
Izorkin <izorkin@elven.pw>
2020-10-19 14:46:36 +0300

    phpPackages.composer: 1.10.13 -> 1.10.15

--- pkgs/development/php/php-packages.nix
+++ pkgs/development/php/php-packages.nix
@@ -97,12 +97,12 @@ let
   };
 
   composer = mkDerivation rec {
-    version = "1.10.13";
+    version = "1.10.15";
     pname = "composer";
 
     src = pkgs.fetchurl {
       url = "https://getcomposer.org/download/${version}/composer.phar";
-      sha256 = "13vhfdlkmpvmk1h30f1i688xk7sdgfj0b82am32jgpa8zmf499sw";
+      sha256 = "1shsxsrc2kq74s1jbq3njn9wzidcz7ak66n9vyz8z8d0hqpg37d6";
     };
 
     dontUnpack = true;
