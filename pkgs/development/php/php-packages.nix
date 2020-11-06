0ac92239e58b6d9767509f4d2182a5ed36e19fba
Izorkin <izorkin@elven.pw>
2020-11-06 17:54:19 +0300

    phpPackages.php-cs-fixer: 2.16.4 -> 2.16.7

--- pkgs/development/php/php-packages.nix
+++ pkgs/development/php/php-packages.nix
@@ -427,12 +427,12 @@ let
   };
 
   php-cs-fixer = mkDerivation rec {
-    version = "2.16.4";
+    version = "2.16.7";
     pname = "php-cs-fixer";
 
     src = pkgs.fetchurl {
       url = "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v${version}/php-cs-fixer.phar";
-      sha256 = "05rdvypxc86hjs8b7id2csa7g1rf7dk2swzfvd5768abdgfasvr8";
+      sha256 = "1azivqvgqy224g2ch9v9qgi31w4ml7fph3bsk8c304yvbvvfv5nh";
     };
 
     phases = [ "installPhase" ];
