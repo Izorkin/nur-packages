e35435f7532aea6ab08d10b47b4dd4fba9640019
Izorkin <izorkin@elven.pw>
2020-12-07 22:05:19 +0300

    phpPackages.composer: return version 1.10.15

--- pkgs/development/php/php-packages.nix
+++ pkgs/development/php/php-packages.nix
@@ -97,6 +97,35 @@ let
   };
 
   composer = mkDerivation rec {
+    version = "1.10.15";
+    pname = "composer";
+
+    src = pkgs.fetchurl {
+      url = "https://getcomposer.org/download/${version}/composer.phar";
+      sha256 = "1shsxsrc2kq74s1jbq3njn9wzidcz7ak66n9vyz8z8d0hqpg37d6";
+    };
+
+    dontUnpack = true;
+
+    nativeBuildInputs = with pkgs; [ makeWrapper ];
+
+    installPhase = with pkgs; ''
+      mkdir -p $out/bin
+      install -D $src $out/libexec/composer/composer.phar
+      makeWrapper ${php}/bin/php $out/bin/composer \
+        --add-flags "$out/libexec/composer/composer.phar" \
+        --prefix PATH : ${lib.makeBinPath [ unzip ]}
+    '';
+
+    meta = with pkgs.lib; {
+      description = "Dependency Manager for PHP";
+      license = licenses.mit;
+      homepage = "https://getcomposer.org/";
+      maintainers = with maintainers; [ globin offline ];
+    };
+  };
+
+  composer2 = mkDerivation rec {
     version = "2.0.7";
     pname = "composer";
 
