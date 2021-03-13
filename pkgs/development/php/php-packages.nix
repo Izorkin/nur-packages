413eaa1660490a8cd2ecee2e8d670fdcdf33497d
Izorkin <izorkin@elven.pw>
2021-03-13 15:50:19 +0300

    phpPackages.imagick: fix build ImageMagick 7

--- pkgs/development/php/php-packages.nix
+++ pkgs/development/php/php-packages.nix
@@ -288,6 +288,11 @@ let
         url = "https://github.com/Imagick/imagick/pull/336.patch";
         sha256 = "nuRdh02qaMx0s/5OzlfWjyYgZG1zgrYnAjsZ/UVIrUM=";
       })
+      # Fix detection of ImageMagick 7.
+      (fetchpatch {
+        url = "https://github.com/Imagick/imagick/commit/09551fbf38c16cdaf4ade7c08744501cd82d2747.patch";
+        sha256 = "qUeQHP08kKOzuQdEpR8RSZ18Yhi0U9z24KwQcAx1UVg=";
+      })
     ];
 
     nativeBuildInputs = with pkgs; [ pkg-config ];
