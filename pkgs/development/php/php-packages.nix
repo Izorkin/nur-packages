26dbfce31fc7bebc792764721c7c03b64526439b
Izorkin <izorkin@elven.pw>
2021-03-13 17:31:02 +0300

    phpPackages.event: 3.0.2 -> 3.0.2r1

--- pkgs/development/php/php-packages.nix
+++ pkgs/development/php/php-packages.nix
@@ -213,10 +213,10 @@ let
   };
 
   event = buildPecl {
-    version = "3.0.2";
+    version = "3.0.2r1";
     pname = "event";
 
-    sha256 = "1ws4l014z52vb23xbsfj6viwkf7fmh462af639xgbp0n6syf77dq";
+    sha256 = "1i92kdh9lrjg8sjqd08p6j6zv9xy6f210zk2i9xpc8fpg1l876ay";
 
     configureFlags = with pkgs; [
       "--with-event-libevent-dir=${libevent.dev}"
