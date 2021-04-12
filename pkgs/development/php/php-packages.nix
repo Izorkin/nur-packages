012816c9b7446b7c2f5ec3cb3e11114c69438811
Izorkin <izorkin@elven.pw>
2021-04-12 15:32:26 +0300

    phpPackages.xdebug: 3.0.3 -> 3.0.4

--- pkgs/development/php/php-packages.nix
+++ pkgs/development/php/php-packages.nix
@@ -996,10 +996,10 @@ let
   };
 
   xdebug30 = buildPecl {
-    version = "3.0.3";
+    version = "3.0.4";
     pname = "xdebug";
 
-    sha256 = "0ci4jgi0klb7a51raj7zfxqm7154s53ff8n95nbpyfcfa60ml9p7";
+    sha256 = "1bvjmnx9bcfq4ikp02kiqg0f7ccgx4mkmz5d7g6v0d263x4r0wmj";
 
     doCheck = true;
     checkTarget = "test";
