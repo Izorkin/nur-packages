41641245ada99635e9e40aecf557db7cd6ff0068
Izorkin <izorkin@elven.pw>
2021-07-29 21:56:45 +0300

    php80: 8.0.8 -> 8.0.9

--- pkgs/development/php/default.nix
+++ pkgs/development/php/default.nix
@@ -464,7 +464,7 @@ in {
   };
 
   php80 = generic {
-    version = "8.0.8";
-    sha256 = "1mqc7i30kak1z7ss5zr3zxfcabxq8ky8d3zl5nschiavc94fmw67";
+    version = "8.0.9";
+    sha256 = "06i73843ilnl0c4z5yvj016zby2g3hmxnci6l32ikpzfgi2jfza6";
   };
 }
