81ec7a9d4347d13765f8b49781acf1ca80c0d381
Izorkin <izorkin@elven.pw>
2021-11-19 15:45:52 +0300

    php56: update backport security bug patches

--- pkgs/development/php/default.nix
+++ pkgs/development/php/default.nix
@@ -379,6 +379,7 @@ in {
       ./patch/php56/php5640-79877.patch
       ./patch/php56/php5640-80672.patch
       ./patch/php56/php5640-80710.patch
+      ./patch/php56/php5640-81026.patch
       ./patch/php56/php5640-81122.patch
       ./patch/php56/php5640-81211.patch
     ];
