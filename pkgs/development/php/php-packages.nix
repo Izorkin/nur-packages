d2d9f833d52c10e7a3d0fb7bbf2aca0eac5df4ae
Izorkin <izorkin@elven.pw>
2022-08-18 12:34:18 +0300

    phpPackages.protobuf: 3.21.2 -> 3.21.5

--- pkgs/development/php/php-packages.nix
+++ pkgs/development/php/php-packages.nix
@@ -757,9 +757,9 @@ let
 
   protobuf321 = buildPecl {
     pname = "protobuf";
-    version = "3.21.2";
+    version = "3.21.5";
 
-    sha256 = "sha256-Csi4OjDLKtJAF8eU3ByUM2BxSDA6SWy5ZgExrkhVrPg=";
+    sha256 = "sha256-B8ytFyUJ8fLBwHmaKXxfOy0h6tRELjqc5IxUUl/YU5w=";
 
     buildInputs = with pkgs; [ (if isPhp73 then pcre2.dev else pcre.dev) ];
 
