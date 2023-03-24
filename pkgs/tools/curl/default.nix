41250c610f15152143ce9c52b5764f2b037f063f
Izorkin <izorkin@elven.pw>
2023-03-24 10:35:37 +0300

    curl: fix passthru tests evaluation

--- pkgs/tools/curl/default.nix
+++ pkgs/tools/curl/default.nix
@@ -28,7 +28,6 @@
 
 # for passthru.tests
 , coeurl
-, curlcpp
 , curlpp
 , haskellPackages
 , ocamlPackages
@@ -171,7 +170,7 @@ stdenv.mkDerivation (finalAttrs: {
     inherit opensslSupport openssl;
     tests = {
       withCheck = finalAttrs.finalPackage.overrideAttrs (_: { doCheck = true; });
-      fetchpatch = tests.fetchpatch.simple.override { fetchpatch = fetchpatch.override { fetchurl = useThisCurl fetchurl; }; };
+      fetchpatch = tests.fetchpatch.simple.override { fetchpatch = (fetchpatch.override { fetchurl = useThisCurl fetchurl; }) // { version = 1; }; };
       curlpp = useThisCurl curlpp;
       coeurl = useThisCurl coeurl;
       haskell-curl = useThisCurl haskellPackages.curl;
