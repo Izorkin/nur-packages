401e5f5a7b3427499d607bbcc54f1731d37c7310
Izorkin <izorkin@elven.pw>
2025-01-12 20:00:52 +0300

    mariadb 10.7: build with fmt v9.x

--- pkgs/servers/mariadb/mariadb_10_7.nix
+++ pkgs/servers/mariadb/mariadb_10_7.nix
@@ -2,7 +2,7 @@
 # Native buildInputs components
 , bison, boost, cmake, fixDarwinDylibNames, flex, makeWrapper, pkg-config
 # Common components
-, curl, fmt_8, libiconv, ncurses, openssl, pcre2
+, curl, fmt_9, libiconv, ncurses, openssl, pcre2
 , libaio, libkrb5, systemd
 , CoreServices, cctools, perl
 , jemalloc, less
@@ -39,7 +39,7 @@ common = rec { # attributes common to both builds
     ++ lib.optional (!stdenv.hostPlatform.isDarwin) makeWrapper;
 
   buildInputs = [
-    curl fmt_8 libiconv ncurses openssl pcre2 zlib
+    curl fmt_9 libiconv ncurses openssl pcre2 zlib
   ] ++ lib.optionals stdenv.hostPlatform.isLinux [ libaio libkrb5 systemd ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [ CoreServices cctools perl ]
     ++ lib.optional (!stdenv.hostPlatform.isDarwin) [ jemalloc ];
