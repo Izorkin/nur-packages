676de54779069ae258a92beb5a2c2e6606d03215
Izorkin <izorkin@elven.pw>
2025-01-18 17:20:15 +0300

    php74: fix build with gcc 14

--- pkgs/development/php/default.nix
+++ pkgs/development/php/default.nix
@@ -323,8 +323,9 @@ let
         CXXFLAGS = toString (lib.optional stdenv.cc.isClang [
           "-std=c++11"
         ]);
-        NIX_CFLAGS_COMPILE = toString (lib.optionals (lib.versions.majorMinor version <= "7.3") [
+        NIX_CFLAGS_COMPILE = toString (lib.optional (lib.versions.majorMinor version <= "7.4") [
           "-Wno-error=incompatible-pointer-types"
+        ] ++ lib.optional (lib.versions.majorMinor version <= "7.3") [
           "-Wno-error=implicit-function-declaration"
         ] ++ lib.optional (lib.versions.majorMinor version <= "7.2") [
           "-Wno-error=implicit-int"
